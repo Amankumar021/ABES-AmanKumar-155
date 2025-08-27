753f9cbd449a69ff55817ca9f885bcf84beeea45
Aman Kumar <aman706839@gmail.com>
2025-08-27 15:49:20 +0530

    attach file to a main directory

---
+++ HTML/login.js
@@ -0,0 +1,8 @@
+document.querySelectorAll('button')[0].addEventListener('click', function() {
+    alert('Log IN button clicked!');
+}); 
+// This code adds a click event listener to the first button on the page.
+
+// When the button is clicked, an alert box will pop up with the message 'Log IN button clicked!'.
+// You can replace the alert with any other functionality you want to implement on button click.
+// Make sure to include this script in your HTML file after the button element to ensure it works correctly.
